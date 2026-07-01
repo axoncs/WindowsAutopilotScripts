4a166508c48046492ca775d4c55a2b5546be5df7
Jesse Vallaro <jesse@axoncs.com>
2026-07-01 17:28:34 -0400

    cleaned up allowed assignment statuses

--- UploadHash.ps1
+++ UploadHash.ps1
@@ -2,13 +2,14 @@ Set-ExecutionPolicy Unrestricted
 Install-Module Microsoft.Graph.Beta.DeviceManagement.Enrollment -force
 install-script get-windowsautopilotinfo -Force
 get-windowsautopilotinfo -online
+$allowedAssignmentStatuses = @("assignedInSync", "assignedOutOfSync", "assignedUnkownSyncState")
 $autoPilotInfo = get-windowsautopilotinfo
 while (($assignmentStatus -eq "") -or ($assignmentStatus -eq $null)) {
     $autoPilotDevice = Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -All | Where-Object SerialNumber -eq $autoPilotInfo."Device Serial Number"
     $assignmentStatus = $autoPilotDevice.DeploymentProfileAssignmentStatus
 }
 [Console]::Clear()
-while ($assignmentStatus -notin "assignedInSync", "assignedOutOfSync", "assignedUnkownSyncState") {
+while ($assignmentStatus -notin $allowedAssignmentStatuses) {
     Write-Host "Device is not assigned yet..."
     $assignmentStatus = (Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -WindowsAutopilotDeviceIdentityId $autoPilotDevice.ID).DeploymentProfileAssignmentStatus
     [Console]::Clear()
