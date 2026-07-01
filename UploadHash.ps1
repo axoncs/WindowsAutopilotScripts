bed3f150f6f869e492b7a179ffe3bce5abbfb724
Jesse Vallaro <jesse@axoncs.com>
2026-07-01 17:19:13 -0400

    added logic to wait for the device info before checking for assignment status.

--- UploadHash.ps1
+++ UploadHash.ps1
@@ -2,14 +2,15 @@ Set-ExecutionPolicy Unrestricted
 Install-Module Microsoft.Graph.Beta.DeviceManagement.Enrollment -force
 install-script get-windowsautopilotinfo -Force
 get-windowsautopilotinfo -online
-
-$autopilotInfo = get-windowsautopilotinfo
-$autoPilotDevice = Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -All | Where-Object SerialNumber -eq $autopilotInfo."Device Serial Number"
-$assignmentStatus = $autoPilotDevice.DeploymentProfileAssignmentStatus
+$autoPilotInfo = get-windowsautopilotinfo
+while (($assignmentStatus -eq "") -or ($assignmentStatus -eq $null)) {
+    $autoPilotDevice = Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -All | Where-Object SerialNumber -eq $autoPilotInfo."Device Serial Number"
+    $assignmentStatus = $autoPilotDevice.DeploymentProfileAssignmentStatus
+}
 [Console]::Clear()
 while ($assignmentStatus -notin "assignedInSync", "assignedOutOfSync", "assignedUnkownSyncState") {
     Write-Host "Device is not assigned yet..."
-    $assignmentStatus = Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -WindowsAutopilotDeviceIdentityId $autoPilotDevice.ID
+    $assignmentStatus = (Get-MgBetaDeviceManagementWindowsAutopilotDeviceIdentity -WindowsAutopilotDeviceIdentityId $autoPilotDevice.ID).DeploymentProfileAssignmentStatus
     [Console]::Clear()
 }
 [Console]::Clear()
